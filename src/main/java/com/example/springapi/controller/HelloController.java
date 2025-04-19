e8632523d7f55a05825437f33664b4015481c77e
AyshenDjamal <ayshen.rasul92@gmail.com>
2025-04-19 18:21:33 +0400

    updated

--- src/main/java/com/example/springapi/controller/HelloController.java
+++ src/main/java/com/example/springapi/controller/HelloController.java
@@ -1,4 +1,4 @@
-/*
+
 package com.example.springapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-        ./mvnw clean package                    jar fayili alir
-        docker build -t my-spring-api .        Docker image yaradir
-        docker run -d -p 8080:8080 --name spring-api-container my-spring-api           Yaranan docker image run edir
+       // ./mvnw clean package                    jar fayili alir
+        //docker build -t my-spring-api .        Docker image yaradir
+        //docker run -d -p 8080:8080 --name spring-api-container my-spring-api           Yaranan docker image run edir
 
 
     @GetMapping("/api/salam-dunya")
@@ -52,4 +52,4 @@ public class HelloController {
         }
     }
 }
-*/
+
